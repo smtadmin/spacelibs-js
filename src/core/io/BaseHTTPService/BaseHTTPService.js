53437d9f58394b9b54b6c3944f2a5a7c4d1fcb01
Tyler Gaffaney <tyler.gaffaney@siliconmtn.com>
2021-02-18 15:21:57 -0700

    fix: 🐛 Removed logging that was left behind

--- src/core/io/BaseHTTPService/BaseHTTPService.js
+++ src/core/io/BaseHTTPService/BaseHTTPService.js
@@ -6,7 +6,7 @@
  * File Created: Monday, 18th January 2021 3:42 pm
  * Author: Tyler Gaffaney (tyler.gaffaney@siliconmtn.com)
  * -----
- * Last Modified: Thursday, 18th February 2021 3:11 pm
+ * Last Modified: Thursday, 18th February 2021 3:21 pm
  * Modified By: tyler Gaffaney (tyler.gaffaney@siliconmtn.com>)
  * -----
  * Copyright 2021, Silicon Mountain Technologies, Inc.
@@ -49,7 +49,6 @@ class BaseHTTPService {
 	 * @memberof BaseHTTPService
 	 */
 	read(url, data, onSuccess, onFailure,  options = {}){
-		console.log("HELLO");
 		this.request(HTTPMethod.GET, url, data, onSuccess, onFailure, undefined, options);
 	}
 	
@@ -138,7 +137,6 @@ class BaseHTTPService {
 		/**
 		 * Add url
 		 */
-
 		requestConfig.url = url;
 		
 		/**
@@ -173,8 +171,6 @@ class BaseHTTPService {
 		 */
 		const cancelTokenSource = axios.CancelToken.source();
 		requestConfig.cancelToken = cancelTokenSource.token;
-
-		console.log(requestConfig);
 		axios.request(requestConfig).then((response) => {
 			// Transform it from an Axios response to whatever
 			onSuccess(response);
