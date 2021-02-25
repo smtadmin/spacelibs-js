226db08cdd8f79d8e90166bacd9fdacfd86d840e
thersis94 <thersis94@gmail.com>
2021-02-25 16:37:32 -0700

    feat: 🎸 Refactored and republished package for the move to
    Refactored and republished package for the move to gitlab.

--- src/core/io/BaseHTTPService/BaseHTTPService.js
+++ src/core/io/BaseHTTPService/BaseHTTPService.js
@@ -2,12 +2,16 @@
  * File: /src/core/io/BaseHTTPService/BaseHTTPService.js
  * Version: 0.0.1
  * Project: spacelibs-js
- * Description: This a generic HTTP Service class. You can construct the class with some optional configuration.  There are convenience methods such as read, insert, update, delete, uploadFile, which all transform the request and call 'request' which is the generic request method.
+ * Description: This a generic HTTP Service class. You can construct the 
+ * class with some optional configuration.  There are convenience methods 
+ * such as read, insert, update, delete, uploadFile, which all transform 
+ * the request and call 'request' which is the generic request method.
+ * 
  * File Created: Monday, 18th January 2021 3:42 pm
  * Author: Tyler Gaffaney (tyler.gaffaney@siliconmtn.com)
  * -----
- * Last Modified: Thursday, 18th February 2021 3:45 pm
- * Modified By: tyler Gaffaney (tyler.gaffaney@siliconmtn.com>)
+ * Last Modified: Thursday, 25th February 2021 4:10 pm
+ * Modified By: Justin Jeffrey (justin.jeffrey@siliconmtn.com>)
  * -----
  * Copyright 2021, Silicon Mountain Technologies, Inc.
  */
@@ -19,14 +23,14 @@ import axios from 'axios';
  * Class representing the BaseHTTPService
  */
 class BaseHTTPService {
-	
+
 	/**
 	 * Constructor for the Base HTTP Service
 	 * @param {object} config - Configuration data that will effect aspects of requests.
 	 * @constructs BaseHTTPService
 	 * @memberof BaseHTTPService
 	 */
-	constructor(config){
+	constructor(config) {
 		// Axios Instance
 		this.axiosInstance = axios.create({
 			baseURL: config.host,
@@ -34,156 +38,138 @@ class BaseHTTPService {
 		});
 
 		this.settings = {
-			requireSSL : config.requireSSL
+			requireSSL: config.requireSSL
 		};
+	}
+
+	/**
+	 * Function to return the baseURL
+	 * @returns {*} The set baseURL
+	 */
+	getBaseURL(){
+		return this.axiosInstance.defaults.baseURL;
 	}
 
 	/**
 	 * Request call for reading/selecting data.  Uses the `GET` HTTP Method.
 	 * @param {string} url - Location for the request to go to.  Can be absolute and relative (if you passed in baseUrl to the constructor)
-	 * @param {object} data - Data for the request
-	 * @param {Function} onSuccess - Handler for successful request.  Handler is passed response object.
-	 * @param {Function} onFailure - Handler for errors/failures with the request.  Handler is passed an error object.
+	 * @param {object} params - Params for the request
+	 * @param {Function} onComplete - Handler for request completion.
 	 * @param {object} options - Additional options for this request.
 	 * @memberof BaseHTTPService
 	 */
-	read(url, data, onSuccess, onFailure,  options = {}){
-		this.request(HTTPMethod.GET, url, data, onSuccess, onFailure, undefined, options);
+	read(url, params, onComplete, options = {}) {
+		this.request(HTTPMethod.GET, url, null, params, onComplete, undefined, options);
 	}
-	
+
 	/**
 	 * Request call for inserting data.  Uses the `POST` HTTP Method.
 	 * @param {string} url - Location for the request to go to.  Can be absolute and relative (if you passed in baseUrl to the constructor)
 	 * @param {object} data - Data for the request
-	 * @param {Function} onSuccess - Handler for successful request.  Handler is passed response object.
-	 * @param {Function} onFailure - Handler for errors/failures with the request.  Handler is passed an error object.
+	 * @param {object} params - Params for the request
+	 * @param {Function} onComplete - Handler for request completion.
 	 * @param {object} options - Additional options for this request.
 	 * @memberof BaseHTTPService
 	 */
-	insert(url, data, onSuccess, onFailure, options = {}){
-		this.request(HTTPMethod.POST, url, data, onSuccess, onFailure, undefined, options);
+	insert(url, data, params, onComplete, options = {}) {
+		this.request(HTTPMethod.POST, url, data, params, onComplete, undefined, options);
 	}
-	
+
 	/**
 	 * Request call for updating existing data.  Uses the `PATCH` HTTP Method.
 	 * @param {string} url - Location for the request to go to.  Can be absolute and relative (if you passed in baseUrl to the constructor)
 	 * @param {object} data - Data for the request
-	 * @param {Function} onSuccess - Handler for successful request.  Handler is passed response object.
-	 * @param {Function} onFailure - Handler for errors/failures with the request.  Handler is passed an error object.
+	 * @param {object} params - Params for the request
+	 * @param {Function} onComplete - Handler for request completion.
 	 * @param {object} options - Additional options for this request.
 	 * @memberof BaseHTTPService
 	 */
-	update(url, data, onSuccess, onFailure, options = {}){
-		this.request(HTTPMethod.PATCH, url, data, onSuccess, onFailure, undefined, options);
+	update(url, data, params, onComplete, options = {}) {
+		this.request(HTTPMethod.PATCH, url, data, params, onComplete, undefined, options);
 	}
 
 	/**
 	 * Request call for deleting data.  Uses the `DELETE` HTTP Method.
 	 * @param {string} url - Location for the request to go to.  Can be absolute and relative (if you passed in baseUrl to the constructor)
 	 * @param {object} data - Data for the request
-	 * @param {Function} onSuccess - Handler for successful request.  Handler is passed response object.
-	 * @param {Function} onFailure - Handler for errors/failures with the request.  Handler is passed an error object.
+	 * @param {object} params - Params for the request
+	 * @param {Function} onComplete - Handler for request completion.
 	 * @param {object} options - Additional options for this request.
 	 * @memberof BaseHTTPService
 	 */
-	delete(url, data, onSuccess, onFailure, options = {}){
-		this.request(HTTPMethod.DELETE, url, data, onSuccess, onFailure, undefined, options);
+	delete(url, data, params, onComplete, options = {}) {
+		this.request(HTTPMethod.DELETE, url, data, params, onComplete, undefined, options);
 	}
 
 	/**
 	 * Request call for uploading a file in a request.  Files are uploaded with `POST` request
 	 * @param {string} url - Location for the request to go to.  Can be absolute and relative (if you passed in baseUrl to the constructor)
 	 * @param {object} file - The javascript file object to upload
-	 * @param {Function} onSuccess - Handler for successful request.  Handler is passed response object.
-	 * @param {Function} onFailure - Handler for errors/failures with the request.  Handler is passed an error object.
+	 * @param {Function} onComplete - Handler for request completion.
 	 * @param {Function} onUploadProgress - Handler for upload progress.  Hanlder is passed a progress object.
 	 * @param {object} options - Additional options for this request.
 	 * @memberof BaseHTTPService
 	 */
-	uploadFile(url, file, onSuccess, onFailure, onUploadProgress, options = {}){
+	uploadFile(url, file, onComplete, onUploadProgress, options = {}) {
 		options.headers = {
-			"Content-Type" : "multipart/form-data"	
+			"Content-Type": "multipart/form-data"
 		};
 		var formData = new FormData();
 		formData.append("file", file);
-		this.request(HTTPMethod.POST, url, formData, onSuccess, onFailure, onUploadProgress, options);
+		this.request(HTTPMethod.POST, url, formData, undefined, onComplete, onUploadProgress, options);
 	}
-	
+
 	/**
 	 * All purpose HTTP Request function
 	 * @param {module:HTTPMethod} method - The method for this HTTP Request
 	 * @param {string} url - The url you are making this request too.  If it's a relative URI, the baseURL you passed in will be used as the base.
-	 * @param {object} data - Data for the request
-	 * @param {Function} onSuccess - Handler for successful request.  Handler is passed response object.
-	 * @param {Function} onFailure - Handler for errors/failures with the request.  Handler is passed an error object.
+	 * @param {object} bodyData - Data for the request
+	 * @param {object} reqParams - Data for the request
+	 * @param {Function} onComplete - Handler for responses. Handler is passed response object.
 	 * @param {any} onUploadProgress - Handler for upload progress.  Hanlder is passed a progress object.
 	 * @param {object} options - Additional options for this request.
 	 * @returns {object} Cancel token to cancel the request
 	 * @memberof BaseHTTPService
-	 */	
-	request(method, url, data, onSuccess, onFailure, onUploadProgress, options) {
-		
-		/**
-		 * Validate method
-		 */
-		if(HTTPMethod[method] === undefined){
-			console.error("HTTP Method " + method + " is not valid.");
-			return;
-		}
-
-
-		let requestConfig = {};
-
-		/**
-		 * Add url
-		 */
-		requestConfig.url = url;
-
-		/**
-		 * Add HTTP Method
-		 */
-		requestConfig.method = HTTPMethod[method];
-
-		/**
-		 * Validate options
-		 */
-		if(options === undefined || options === null){
-			console.warn("Request options was null, by default it should be an empty object {}");
-		}else if(options["headers"] !== undefined){
-			requestConfig.headers = options.headers;
-		}
-
-		/**
-		 * HTTP Method handing
-		 */
-		if(data !== undefined && data !== null){
-			if(method === HTTPMethod.POST || method === HTTPMethod.PATCH || method === HTTPMethod.PUT || method === HTTPMethod.DELETE){
-				requestConfig.data = data;
-			}else{
-				requestConfig.params = data;
-			}
-		}
-
-		/**
-		 * Upload Progress handler
-		 */
-		if(onUploadProgress !== undefined){
-			requestConfig.onUploadProgress = onUploadProgress;
-		}
-
+	 */
+	request(method, url, bodyData, reqParams, onComplete, onUploadProgress, options) {
 		/**
 		 * Cancel tokens
 		 */
 		const cancelTokenSource = axios.CancelToken.source();
-		requestConfig.cancelToken = cancelTokenSource.token;
-		axios.request(requestConfig).then((response) => {
+
+		let requestConfig = {
+			url: url,
+			method: HTTPMethod[method],
+			headers: options.headers,
+			data: bodyData,
+			params: reqParams,
+			onUploadProgress: onUploadProgress,
+			cancelToken: cancelTokenSource.token
+		};
+
+		this.axiosInstance.request(requestConfig).then((response) => {
 			// Transform it from an Axios response to whatever
-			onSuccess(response);
+			onComplete(this.tranformSuccess(response));
 		}).catch((error) => {
 			// Transfrom from Axios Error to whatever
-			onFailure(error);
+			onComplete(this.transformError(error));
 		});
+
 		return cancelTokenSource;
+	}
+
+	tranformSuccess(response) {
+		return {
+			isValid: true,
+			data: response.data
+		};
+	}
+
+	transformError(error) {
+		return {
+			isValid: false,
+			data: null
+		};
 	}
 }
 
