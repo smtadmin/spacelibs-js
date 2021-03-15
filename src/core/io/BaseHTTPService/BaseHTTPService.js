02409c8dcf3c0354d94d2d061a121888b7d1e4da
thersis94 <thersis94@gmail.com>
2021-03-15 16:59:04 -0600

    feat: 🎸 updating the respose transform methods to reflect the
    updating the respose transform methods to reflect the changes made on
    the back end

--- src/core/io/BaseHTTPService/BaseHTTPService.js
+++ src/core/io/BaseHTTPService/BaseHTTPService.js
@@ -10,7 +10,7 @@
  * File Created: Monday, 18th January 2021 3:42 pm
  * Author: Tyler Gaffaney (tyler.gaffaney@siliconmtn.com)
  * -----
- * Last Modified: Thursday, 25th February 2021 4:55 pm
+ * Last Modified: Monday, 15th March 2021 4:56 pm
  * Modified By: Justin Jeffrey (justin.jeffrey@siliconmtn.com>)
  * -----
  * Copyright 2021, Silicon Mountain Technologies, Inc.
@@ -140,7 +140,7 @@ class BaseHTTPService {
 		let requestConfig = {
 			url: url,
 			method: HTTPMethod[method],
-			headers: {Authorization: "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwdklQUHpzdXR3NmZtV0xOT1ZKdzZQb25tY1gtWFpobm05Y2pPMV9EYUdzIn0.eyJleHAiOjE2MTQyOTYwNTMsImlhdCI6MTYxNDI5NTc1MywianRpIjoiMTI2ZmU3NTktN2I0Yi00YjJkLTkwYjctYmQ5NzVlYjc0NTUxIiwiaXNzIjoiaHR0cHM6Ly9hcnRpZmljaWFsaXMtbWFjaGluYS5oNGNrLm1lL2F1dGgvcmVhbG1zL2V6Zm9ybSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI5ODQ0MWMwMy0yM2NmLTRhZmUtYmU3NS1hYzI3MWE0NWMyOTUiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJlemZvcm0tYXBpIiwic2Vzc2lvbl9zdGF0ZSI6ImE0MDY0ZTk0LWZkMjgtNGQ0Mi1hOWNmLTViYTVkNjk3MjdiMiIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImV6Zm9ybS1hcGkiOnsicm9sZXMiOlsiTWVtYmVyIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJqYW1lcyBqYW1lcyIsInByZWZlcnJlZF91c2VybmFtZSI6ImV0ZXdhIiwiZ2l2ZW5fbmFtZSI6ImphbWVzIiwiZmFtaWx5X25hbWUiOiJqYW1lcyIsImVtYWlsIjoiamFtZXNAc210LmNvbSJ9.WMiZ7bfzBbXTb4FN_NhZzBTMfJ0Z47VoTLViYAxngYfzvJC2O94jUu-CISRDrD-mu5_DRLCKVFuCAuWpl1qY-1SLTy8YYjWm6wc8_fz0NJ8557TP2-skBtLphlv5FuUTDYJXxdABgt2opbHbsju4xYxkaA_NpW6JO9CwdFHudk4y1JkZ-PfQDq7WrRZKRX0rSwnOu5vqgr_silHyddBuUVcUwTccoIj_QqTRwgXibKG85G0PpkutSDV7wwbkal7CD6C9QsIC5GpDej0MSrE35Hyy8dVaCFP8775O8xyjgw-PMw0Cg-NFFk6aNZsJfI7__YmRE5NrZwyB_EYAsyE-YA"},
+			headers: options?.headers,
 			data: bodyData,
 			params: reqParams,
 			onUploadProgress: onUploadProgress,
@@ -158,13 +158,27 @@ class BaseHTTPService {
 		return cancelTokenSource;
 	}
 
+	/**
+	 * Returns a standard response object
+	 *
+	 * @param {*} response Response object
+	 * @returns {*} Response object
+	 * @memberof BaseHTTPService
+	 */
 	tranformSuccess(response) {
 		return {
 			isValid: true,
-			data: response.data
+			data: response.data.data
 		};
 	}
 
+	/**
+	 * Returns a standard error object
+	 *
+	 * @param {*} error Error object
+	 * @returns {*} Response object
+	 * @memberof BaseHTTPService
+	 */
 	transformError(error) {
 		return {
 			isValid: false,
